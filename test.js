ee62df79c883c4fa356f0a15d595389b6f2420fe
Vinicius Rodrigues <166398236+Rodrigues-Silva@users.noreply.github.com>
2024-10-27 23:29:37 -0300

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-function toggleComments() {
-    const commentsSection = document.getElementById("comments")
-
-    if (commentsSection.style.display == "none" || commentsSection.style.display == "") {
-        commentsSection.style.display = "block";
-    } else {
-        commentsSection.style.display = "none";
-    }
-}
