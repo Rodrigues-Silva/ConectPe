a51eff0716f617f63177f6ec906beaa354ace39a
Vinícius Rodrigues <rodriguesdasilvav408@gmail.com>
2024-11-06 21:45:49 -0300

    Atualização no arquivo de inserção, agora com seu bug corrigido, assim recebendo as informações corretamente para inserir ao banco de dados

--- user/insert.php
+++ user/insert.php
@@ -17,10 +17,22 @@
         return password_hash($Pass . $salt, PASSWORD_DEFAULT);
     }
 
+    function getID($PDO, $Name)
+    {
+        $SearchQuery = "SELECT id FROM users WHERE name = :SearchName LIMIT 1";
+        $Query = $PDO->prepare($SearchQuery);
+        //
+        $Query->bindParam(":SearchName", $Name, PDO::PARAM_STR);
+        $Query->execute();
+        //
+        $id = $Query->fetchColumn();
+        return $id;
+    }
+
 
     $Salt = generateSalt();
 
-    $sql = "INSERT INTO users (name, email, keyword, salt) VALUES (:nome, :email, :senha, :salt)";
+    $sql = "INSERT INTO users (name, email, password, salt) VALUES (:nome, :email, :senha, :salt)";
     $query = $pdo->prepare($sql);
 
     $query->bindValue(":nome", $MetaData["Name"]);
@@ -28,14 +40,16 @@
     $query->bindValue(":senha", generatePass($MetaData["Pass"], $Salt));
     $query->bindValue(":salt", $Salt);
 
-    if ($query->execute()) 
-    {
+    if($query->execute()) 
+    {   
+        $_SESSION["ID"] = getID($pdo, $MetaData["Name"]);
+        $_SESSION["Name"] = $MetaData["Name"];
         header("Location: index.php");
     }
-?>
+// ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
