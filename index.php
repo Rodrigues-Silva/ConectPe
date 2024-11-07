89271c8ee1a73305ef83e72767bd0e6034de9f18
Vinícius Rodrigues <rodriguesdasilvav408@gmail.com>
2024-11-06 21:37:33 -0300

    Agora este arquivo verifica se há uma sessão ativa e com  relação no resultado direciona o usuario ou para a tela de login ou a tela principal

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
 
-    header("Location: user/index.php");
-
-?>
+    if($_SESSION) 
+    {
+        header("Location: user/index.php");
+    }
+    else
+    {
+        header("Location: user/formLogin.php");
+    }
