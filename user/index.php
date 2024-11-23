611241f0861c1d75d3e82055d3322d389335f271
Vinícius Rodrigues <rodriguesdasilvav408@gmail.com>
2024-11-23 17:04:16 -0300

    Ajuestes nos elementos na tela, adição de funcionalidades como postar um post, adicionar fotos, acessar perfil de outras pessoas e o proprio

--- user/index.php
+++ user/index.php
@@ -1,12 +1,35 @@
 <?php 
     require_once "../core/connection.php";
     session_start();
+    date_default_timezone_set('America/Sao_Paulo');
+
+    function tempoPassado($dataCriacao) {
+        $agora = new DateTime(); 
+        $criacao = new DateTime($dataCriacao); 
+        $diferenca = $agora->diff($criacao);   
+        // print_r($diferenca);
+        if ($diferenca->d > 0) {
+            return $diferenca->d . ' dia' . ($diferenca->d > 1 ? 's' : '') . ' atrás';
+        } elseif ($diferenca->h > 0) {
+            return $diferenca->h . ' hora' . ($diferenca->h > 1 ? 's' : '') . ' atrás';
+        } elseif ($diferenca->i > 0) {
+            return $diferenca->i . ' minuto' . ($diferenca->i > 1 ? 's' : '') . ' atrás';
+        } else {
+            return 'Agora mesmo';
+        }
+    }
 
     if (!isset($_SESSION["ID"]) && empty($_SESSION["ID"]))
     {
         header("Location: formLogin.php");
         exit();
     }
+
+    $query = "SELECT posts.*, GROUP_CONCAT(midia.media_url SEPARATOR ':') AS img_urls, users.name AS user_name, users.profile_pic AS user_pic FROM posts LEFT JOIN midia ON posts.id = midia.post_id LEFT JOIN users ON posts.user_id = users.id GROUP BY posts.id ORDER BY posts.create_at DESC LIMIT 30;";
+    $consulta =  $pdo->prepare($query);
+    $consulta->execute();
+
+    $posts = $consulta->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -18,12 +41,13 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/posts.css">
 
 </head>
 <body>
     
     <header class="top-bar">
-        <div class="logo">
+        <div class="logo" style="cursor: default;">
             <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-handshake h-8 w-8 text-blue-500" data-id="5"><path d="m11 17 2 2a1 1 0 1 0 3-3"></path><path d="m14 14 2.5 2.5a1 1 0 1 0 3-3l-3.88-3.88a3 3 0 0 0-4.24 0l-.88.88a1 1 0 1 1-3-3l2.81-2.81a5.79 5.79 0 0 1 7.06-.87l.47.28a2 2 0 0 0 1.42.25L21 4"></path><path d="m21 3 1 11h-2"></path><path d="M3 3 2 14l6.5 6.5a1 1 0 1 0 3-3"></path><path d="M3 4h8"></path></svg>
             <span><span class="conect">Conect</span>Pe</span>
         </div>
@@ -93,26 +117,22 @@
                     Espaço para Anúncios
                 </p>
             </div>
+            <button class="anun" type="button">
+                <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
+                Anunciar
+            </button>
         </aside>
         
         <main class="main-content" id="main">
-        <form action="" method="post" enctype="multipart/form-data" class="ADDPost">
+        <form action="../posts/new.php" method="post" enctype="multipart/form-data" class="ADDPost" id="ADDPost">
                 <div class="post-header">
-                    <img alt="User Image" height="40"
+                    <img alt="User Image" height="50"
                     src="<?php echo $_SESSION["profile_pic"]; ?>"
-                    style="border-radius: 50%; margin-right: 10px;" width="40" />
+                    style="border-radius: 50%; margin-right: 10px;" width="50" />
                     <div class="post-info">
                         <span>
                             <?php echo $_SESSION["Name"]?>
                         </span>
-                        <div class="menu">
-                            <i class="fas fa-ellipsis-h"></i>
-                            <div class="menu-content">
-                                <a href="#">
-                                    Acessar Perfil do Usuário
-                                </a>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <div class="post-content">
@@ -141,15 +161,85 @@
                 </div>
             </form>
             <div class="content">
-                
+                <?php foreach($posts as $post) {?>
+                    <div class="post" id="post" data-id="<?php echo $post->$id?>">
+                        <div class="header">
+                            <img alt="User Image" height="50"
+                            src="<?php echo $post->user_pic ?>"
+                            style="border-radius: 50%; margin-right: 10px;" width="50" />
+                            <div class="post-info">
+                                <div class="title">
+                                    <span>
+                                        <?php echo $post->user_name?>
+                                    </span>
+                                    <span class="time"><?php echo tempoPassado($post->create_at);?></span>
+                                </div>
+                                <div class="menu">
+                                    <i class="fas fa-ellipsis-h"></i>
+                                    <div class="menu-content">
+                                        <?php if ($post->user_id != $_SESSION["ID"]) {?>
+                                            <a href="usuario.php?fuy=<?php echo $post->user_id?>">
+                                                Acessar Perfil do Usuário
+                                            </a>
+                                        <?php } else {?>
+                                            <a href="../posts/delete.php?fuy=<?php echo $post->id?>" id="removePost" style="color: red;">
+                                                Deletar post
+                                            </a>
+                                        <?php }?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="content-publi">
+                            <?php echo $post->content;?>
+                            <div class="zone-img">
+                                <?php 
+                                    if ($post->img_urls !== null)
+                                    {
+                                        $imagens = explode(":", $post->img_urls);
+
+                                        foreach ($imagens as $img) {
+                                ?>
+
+                                    <div class="picture">
+                                        <img draggable="false" class="postImg" src="<?php echo "../posts/" . $img?>">
+                                    </div>
+
+                                <?php }} ?>
+                            </div>
+                        </div>
+                        <div class="actions">
+                            <button class="like-button" onclick="toggleLike(this)">
+                                <i class="fas fa-heart">
+                                </i>
+                                Like
+                            </button>
+                            <button>
+                                <i class="fas fa-retweet">
+                                </i>
+                                Retweet
+                            </button>
+                            <button class="button-comment">
+                                <i class="fas fa-comment">
+                                </i>
+                                Comentários
+                            </button>
+                            <button>
+                                <i class="fas fa-share">
+                                </i>
+                                Compartilhar
+                            </button>
+                        </div>
+                    </div>
+                <?php }?>
             </div>
         </main>
         
         <aside class="sidebar-right">
+            <h3>
+                Recomendados
+            </h3>
                 <div class="recommended">
-                    <h3>
-                        Recomendados
-                    </h3>
                     <a href="#">
                         <i class="fas fa-star">
                         </i>
@@ -160,11 +250,36 @@
                         </i>
                         Item Recomendado 2
                     </a>
+                    <a href="#">
+                        <i class="fas fa-star">
+                        </i>
+                        Item Recomendado 3
+                    </a>
+                    <a href="#">
+                        <i class="fas fa-star">
+                        </i>
+                        Item Recomendado 4
+                    </a>
+                    <a href="#">
+                        <i class="fas fa-star">
+                        </i>
+                        Item Recomendado 5
+                    </a>
+                    <a href="#">
+                        <i class="fas fa-star">
+                        </i>
+                        Item Recomendado 5
+                    </a>
+                    <a href="#">
+                        <i class="fas fa-star">
+                        </i>
+                        Item Recomendado 5
+                    </a>
                 </div>
+                <h3>
+                    Hashtags Populares
+                </h3>
                 <div class="hashtags">
-                    <h3>
-                        Hashtags Populares
-                    </h3>
                     <a href="#">
                         <i class="fas fa-hashtag">
                         </i>
@@ -175,23 +290,41 @@
                         </i>
                         #Hashtag2
                     </a>
+                    <a href="#">
+                        <i class="fas fa-hashtag">
+                        </i>
+                        #Hashtag3
+                    </a>
                 </div>
                 <div class="terms">
-                    <h3>
-                        Termos de Uso
-                    </h3>
-                    <a href="#">
-                        Leia nossos termos de uso
-                    </a>
+                    <p id="p_term"><a id="term" href="../terms.html" target="_blank">Termos de Uso</a> - <a id="term" class="term" href="../policy.html" target="_blank">Politica de privacidade</a></p>
+                    <p id="p_term">Versão do App 1.0.0</p>
+                    <p id="p_term">© 2024 ConectPE - Todos os direitos reservados.</p>
                 </div>
         <button class="fixed-button" onclick="scrollToTop()">
             <i class="fas fa-arrow-up">
             </i>
         </button>
         </aside>
+
+    </div>
+    <div class="confirm" id="caixa">
+        <div class="caixinha">
+            <header>
+                <h1>Excluir Post</h1>
+            </header>
+            <main>
+                <p>Deseja realmente excluir este post?</p>
+            </main>
+            <footer>
+                <button id="cancel">Cancelar</button>
+                <button id="confirm">Confirmar</button>
+            </footer>
+        </div>
     </div>
 
     <script src="../responsive/post-img.js"></script>
+    <script src="../posts/fetch.js"></script>
     <script>
 
         function scrollToTop() {
