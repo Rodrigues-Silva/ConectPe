f1d9938b1948e8f29051637022e2d9105f1ffc78
Vinícius Rodrigues <rodriguesdasilvav408@gmail.com>
2024-11-23 16:43:30 -0300

    Agora a sessão fica salva mesmo ao fechar navegador

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php 
+    $lifetime = 864000;
+
+    session_set_cookie_params([
+        'lifetime' => time() + $lifetime, // Duração inicial do cookie
+        'path' => '/',           // Disponível em todo o site
+        'domain' => '',          // Domínio atual
+        'secure' => false,       // Alterar para true em HTTPS
+        'httponly' => true       // Restringe acesso ao cookie via HTTP
+    ]);
+
+    session_start();
+
     if($_SESSION) 
     {
         header("Location: user/index.php");
