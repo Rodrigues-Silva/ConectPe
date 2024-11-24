01731067ed87ff562128425b5c657a521dc06fd9
Vinícius Rodrigues <rodriguesdasilvav408@gmail.com>
2024-11-24 20:49:10 -0300

    backup do banco de dados atualizado

--- conectpe.sql
+++ conectpe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21/11/2024 às 03:51
+-- Tempo de geração: 25/11/2024 às 00:47
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -33,6 +33,13 @@ CREATE TABLE `midia` (
   `media_url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `midia`
+--
+
+INSERT INTO `midia` (`id`, `post_id`, `media_url`) VALUES
+(17, 67300022, 'post-img/6743683258d57.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +52,13 @@ CREATE TABLE `posts` (
   `content` text NOT NULL,
   `create_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `content`, `create_at`) VALUES
+(67300022, 6, 'Primeira postagem oficial do ConectPe', '2024-11-24 17:53:54');
 
 -- --------------------------------------------------------
 
@@ -75,6 +89,14 @@ CREATE TABLE `users` (
   `link_profile` varchar(255) DEFAULT NULL,
   `create_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `salt`, `profile_pic`, `bios`, `link_profile`, `create_at`) VALUES
+(6, 'Admin', 'conectpe0@gmail.com', '$2y$10$R0t8klyK93cCIs3tLqwb7eMG.q4Tlk2SsSOazFyDTZMc11Er4QSc2', '674367eb01a16', '../Midia/6743682315fb7.jpg', 'Criador e gerenciador do projeto &#34;ConectPe&#34;', 'https://github.com/Rodrigues-Silva', '2024-11-24 17:52:43'),
+(7, 'Vinicius R', 'veniceus518@gmail.com', '$2y$10$asOuMAoRJ96OEMqrkEaAfelgrc0bdNzpYnB9RwvGRWSYvSkrkz4OC', '6743b19fee6dc', '../Midia/perfil.png', NULL, NULL, '2024-11-24 23:07:12');
 
 --
 -- Índices para tabelas despejadas
@@ -118,13 +140,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `midia`
 --
 ALTER TABLE `midia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67300009;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67300023;
 
 --
 -- AUTO_INCREMENT de tabela `retweets`
@@ -136,7 +158,7 @@ ALTER TABLE `retweets`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restrições para tabelas despejadas
